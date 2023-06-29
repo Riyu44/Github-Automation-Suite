8cca5b89c77299a7b7eac365547009e59acb11b8
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-29 13:52:45 +0530

    Update read_comment.sh

--- read_comment.sh
+++ read_comment.sh
@@ -5,7 +5,7 @@ TOKEN=$token
 echo "Here's the new comment that I found:"
 
 # Extract the username and repository name from the GitHub URL
-URL="https://github.com/Sopra-Banking-Software-Interns/unified-automation-suite"
+URL="https://github.com/Riyu44/Github-Automation-Suite"
 USERNAME=$(echo "$URL" | sed -n 's#https://github.com/\([^/]*\)/\([^/]*\).*#\1#p')
 REPO=$(echo "$URL" | sed -n 's#https://github.com/\([^/]*\)/\([^/]*\).*#\2#p')
 
