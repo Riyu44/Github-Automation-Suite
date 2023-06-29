c13bdad91f11fbf32068eab8fc1e73cd3f1d50c9
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-29 13:51:47 +0530

    Update contributions.sh

--- contributions.sh
+++ contributions.sh
@@ -1,10 +1,10 @@
 #GET the contributors list from GIT Hub
-REPO="Sopra-Banking-Software-Interns/Github-Learderboard"
+REPO="Riyu44/Github-Automation-Suite"
 curl -s -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $token"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/Sopra-Banking-Software-Interns/unified-automation-suite/contributors | jq -r '.[] | {login, contributions}' >> contributions.txt
+  https://api.github.com/repos/Riyu44/Github-Automation-Suite/contributors | jq -r '.[] | {login, contributions}' >> contributions.txt
 
 #Create a new game on the leaderboard on Cloud storage
 ID=$(curl -s -X POST -H "Content-Type: application/json" -d '{"name": "$REPO"}' https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ | jq -r '.result | scan("Game with ID: (.+) added.")[]')
@@ -20,8 +20,8 @@ rm contributions.json
 # Create a database for the leaderboard
 # Read the JSON file into a variable
 json=$(cat contribution_final.json)
-OWNER="Sopra-Banking-Software-Interns"
-REPO="unified-automation-suite"
+OWNER="Riyu44"
+REPO="Riyu44/Github-Automation-Suite"
 
 # Make a request to fetch closed issues of a contributor
 response=$(curl -s -L \
